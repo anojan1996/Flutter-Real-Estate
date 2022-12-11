55e06e72230ffb20c8313e997a4bbbaf888b0ca1
anojan1996 <anojan2012@gmail.com>
2022-12-11 22:44:06 +0530

    Profile Screen

--- lib/Screens/Profile_Screen.dart
+++ lib/Screens/Profile_Screen.dart
@@ -1,4 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+import '../Backend/Auth.dart';
+import '../Utils/Utils.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -8,10 +13,77 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  var userData = {};
+  bool isLoading = false;
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  getData() async {
+    setState(() {
+      isLoading = true;
+    });
+    try {
+      User currentuser = FirebaseAuth.instance.currentUser!;
+      var userSnap = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(currentuser.uid)
+          .get();
+
+      userData = userSnap.data()!;
+    } catch (e) {
+      showSnackBar(
+        context,
+        e.toString(),
+      );
+    }
+    print(userData['username']);
+    setState(() {
+      isLoading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Profile Screen'),
+    return  isLoading
+        ? const Center(
+            child: CircularProgressIndicator(),
+          )
+        :Scaffold(
+      appBar: AppBar(
+        title: Text('Profile Screen'),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 64,
+                  backgroundImage:
+                      NetworkImage('https://i.stack.imgur.com/l60Hf.png'),
+                  backgroundColor: Colors.red,
+                ),
+                SizedBox(
+                  width: 60,
+                ),
+                Column(
+                  children: [
+                    Text(userData['username']),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(userData['email']),
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:real_estate/Screens/Login_Screen.dart';
 
+import 'Screens/Profile_Screen.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
